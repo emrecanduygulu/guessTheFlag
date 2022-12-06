3f4f0736865002b8e6dd9a1476c14e0e92fc8493
Emre Can Duygulu <ecduygulu@gmail.com>
2022-12-06 18:01:46 +0300

    initial commit

--- guessTheFlag/ContentView.swift
+++ guessTheFlag/ContentView.swift
@@ -9,11 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
+        VStack(spacing: 20) {
             Text("Hello, world!")
+            Text("Emre")
         }
         .padding()
     }
