4df9c621cad43afcf829d4a33d1838efb1720076
Emre Can Duygulu <ecduygulu@gmail.com>
2022-12-06 17:21:50 +0300

    Initial Commit

---
+++ guessTheFlag/guessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  guessTheFlagApp.swift
+//  guessTheFlag
+//
+//  Created by emre can duygulu on 6.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct guessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
