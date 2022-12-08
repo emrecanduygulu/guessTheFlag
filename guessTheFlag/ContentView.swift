04ff79e40fe496227a41f1260e36790b6b4cf2d6
Emre Can Duygulu <ecduygulu@gmail.com>
2022-12-09 02:41:56 +0300

    score has been added

--- guessTheFlag/ContentView.swift
+++ guessTheFlag/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     @State private var showingScore = false
     @State private var scoreTitle = ""
+    @State private var score = 0
     
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
@@ -56,7 +57,7 @@ struct ContentView: View {
                 
                 Spacer()
                 
-                Text("Score: ???")
+                Text("Score: \(score)")
                     .foregroundColor(.white)
                     .font(.title.bold())
                 
@@ -67,15 +68,17 @@ struct ContentView: View {
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
         } message: {
-            Text("Your score is ???")
+            Text("Your score is \(score)")
         }
     }
     
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
+            score += 1
             scoreTitle = "Correct"
+            
         } else {
-            scoreTitle = "Wrong"
+            scoreTitle = "Wrong, the correct flag is \(countries[correctAnswer])"
         }
         
         showingScore = true
